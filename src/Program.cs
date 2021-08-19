3dd0d234f946380efb60d68bdb4d09d370e6102f
manuelspinto <manuelseixaspinto@gmail.com>
2021-08-19 18:09:57 +0100

    Registered DI services
    Updated Program.cs with the DI service registration

--- src/Program.cs
+++ src/Program.cs
@@ -2,8 +2,12 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Azure.Functions.Worker.Configuration;
+using Function.Domain.Services;
+using Function.Domain.Services.HttpClients;
+using Function.Domain.Providers;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace azure_function_example_csharp
+namespace Example.Function
 {
     public class Program
     {
@@ -11,6 +15,12 @@ namespace azure_function_example_csharp
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(s =>
+                {
+                    s.AddScoped<IFinhubDataMapper, FinhubDataMapper>();
+                    s.AddScoped<IStockDataProvider, FinhubProvider>();
+                    s.AddHttpClient<FinhubHttpClient>();
+                })
                 .Build();
 
             host.Run();
