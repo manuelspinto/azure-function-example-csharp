ef6e5ca604643fe93ec9592bb2a095058ad115cc
manuelspinto <manuelseixaspinto@gmail.com>
2021-08-31 21:52:07 +0100

    Added unit tests for the FinhubDataMapper class

---
+++ tests/unit-tests/Function.Domain/Services/FinhubDataMapperTests.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using Function.Domain.Models;
+using Function.Domain.Services;
+
+namespace UnitTests.Function.Domain.Services
+{
+    public class FinhubDataMapperTests
+    {
+        [Fact]
+        public void MapToStockData_ShouldCorrectlyMapTheInput()
+        {
+            //arrange
+            var finHubStockDataInput = new FinhubStockData(){
+                o = 111.1M,
+                h = 222.2M,
+                l = 333.3M,
+                c = 444.4M,
+                pc = 555.5M
+            };
+
+            var sut = new FinhubDataMapper();
+
+            //act
+            var result = sut.MapToStockData(finHubStockDataInput);
+
+            //assert
+            Assert.Equal(111.1M, result.Open);
+            Assert.Equal(222.2M, result.High);
+            Assert.Equal(333.3M, result.Low);
+            Assert.Equal(444.4M, result.Current);
+            Assert.Equal(555.5M, result.PreviousClose);
+        }
+    }
+}
+
